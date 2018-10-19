3ce33fa7850e4b926ca39d53f31e2fe8ae794eca
Shubham <princeshubham22699@gmail.com>
2018-10-19 19:50:12 +0530

    Logout added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fileUpload = require('express-fileupload');
 const expressSession = require('express-session');
 const connectMongo = require('connect-mongo');
 const connectFlash = require('connect-flash');
+const edge = require('edge.js');
 
 const app = new express();
 mongoose.connect('mongodb://localhost/node-blog');
@@ -30,26 +31,34 @@ const createUserController = require('./controllers/createUser');
 const storeUserController = require('./controllers/storeUser');
 const loginController = require('./controllers/login');
 const loginUserController = require('./controllers/loginUser');
+const logoutController = require('./controllers/logout');
 
 app.use(fileUpload());
 app.use(express.static('public'));
 app.use(expressEdge);
 app.set('views' , path.resolve(__dirname , 'views'));
 
+app.use('*' , (req , res , next) => {
+    edge.global('auth' , req.session.userId);
+    next();
+})
+
 app.use(bodyParser.json());             // for decoding the file send as json format fron browser
 app.use(bodyParser.urlencoded({extended : true}));       // for decoding similar to above combined to have favourable result
 
 const storePost = require('./middleware/storePost');
 const auth = require('./middleware/auth');
+const redirectIfAuthenticated = require('./middleware/redirectIfAuthenticated');
 
 app.get('/' , homePageController);
 app.get('/post/new' , auth , createPostController);                         //authorize user
 app.post('/posts/store' , auth , storePost , storePostController);          // validate post
-app.get('/post/:id' , getPostController);
-app.get('/auth/login' , loginController);
-app.post('/users/login' , loginUserController);
-app.get('/auth/register' , createUserController);
-app.post('/users/register', storeUserController);
+app.get('/post/:id' , redirectIfAuthenticated, getPostController);
+app.get('/auth/logout' , logoutController);
+app.get('/auth/login' , redirectIfAuthenticated, loginController);
+app.post('/users/login' , redirectIfAuthenticated, loginUserController);
+app.get('/auth/register' , redirectIfAuthenticated, createUserController);
+app.post('/users/register', redirectIfAuthenticated, storeUserController);
 
 
 app.listen(4000,()=>{
